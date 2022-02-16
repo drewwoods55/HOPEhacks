264e4a1dd7dac98263f3afc096d865c62b7a1702
Andrew  Woods <andrewwoods@Andrews-MacBook-Pro.local>
2022-02-16 10:25:16 -0500

    pushing first attempt with yelp api

---
+++ yelp.js
@@ -0,0 +1,27 @@
+let yelpAPI = require('yelp-api')
+
+// Create a new yelpAPI object with your API key
+let apiKey = process.env.APIKEY
+let yelp = new yelpAPI(apiKey)
+
+// Set any parameters, if applicable (see API documentation for allowed params)
+let params = [{ location: 'Charlotte, NC', categories: 'restaurants,vegan,vegetarian' }]
+
+// Call the endpoint
+yelp.query('businesses/search', params)
+.then(data => {
+  // Success
+  let response = JSON.parse(data)
+  let matches = response.businesses
+
+  console.log(data)
+
+//   for(let i = 0; i < matches.length; i++) {
+//     console.log(matches[i].name)
+//   }
+  
+})
+.catch(err => {
+  // Failure
+  console.log(err)
+});
