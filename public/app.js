ed0eb86facf37a9c8d5aeda9ac49e9ec69b53245
Andrew  Woods <andrewwoods@Andrews-MacBook-Pro.local>
2022-02-19 14:52:24 -0500

    worked with targeting api to show. trying to fetch it so it shows on html

--- public/app.js
+++ public/app.js
@@ -1,7 +1,24 @@
-const menu = document.querySelector('3mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+// const menu = document.querySelector('3mobile-menu')
+// const menuLinks = document.querySelector('.navbar__menu')
 
-menu.addEventListener('click', function(){
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-})
+// const { response } = require("express")
+
+// menu.addEventListener('click', function(){
+//     menu.classList.toggle('is-active');
+//     menuLinks.classList.toggle('active');
+// })
+
+// we will have to call this api and display results on the client side page
+// fetch("/api").then(res => { return res.json() }).then(data => { 
+    // loop through data. everytime through loop is one result. and we choose to show one result or all
+// console.log (data) })
+
+function fetchData(){
+  fetch("http://localhost:8080/api").then(resp =>{
+      const data = response.json();
+      console.log(data)
+
+    
+  })
+}
+fetchData()
