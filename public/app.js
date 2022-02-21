18b97b1e1b342b9dc1f230c215dcdf002e1f9fd5
Seth Mulia <smulia@r2hstudent.com>
2022-02-21 09:21:45 -0500

    removed get started, colored and resized buttons

--- public/app.js
+++ public/app.js
@@ -1,8 +1,13 @@
 const menu = document.querySelector('3mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
-
+const mainBtn = document.querySelector()
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
 
+function toSwipe() {
+    document.getElementById('#mainbtn').onclick = function () {
+        location.href = ""
+    }
+}
